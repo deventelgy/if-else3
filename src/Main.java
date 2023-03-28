dbcd380750ceb240cf58708ab33709415a035f76
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-28 15:19:00 -0500

    Incorporacion de JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
-
+import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
 
+        /*
+        Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un numero");
         int numero=sc.nextInt();
 
@@ -16,6 +17,18 @@ public class Main {
         }else{
             System.out.println("El numero "+numero+" no es divisible entre 2");
             System.out.println("Residuo es: "+(numero%2));
+        }
+        * */
+
+        String texto=JOptionPane.showInputDialog("Introduce un numero");
+        //Pasamos el String a int
+        int numero=Integer.parseInt(texto);
+
+        //Comprobamos si es divisible entre 2, es decir, si el resto de la division es 0
+        if (numero%2==0){
+            System.out.println("El numero "+numero+" es divisible entre 2");
+        }else{
+            System.out.println("El numero "+numero+" no es divisible entre 2");
         }
     }
 }
